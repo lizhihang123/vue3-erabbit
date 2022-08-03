7e256654bef6aa58f7764ad288ab19ead84b8321
lizhihang123 <2420554367@qq.com>
2022-08-03 18:44:13 +0800

    feat: 新增跨域文件

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -5,11 +5,11 @@ export default function (req, res) {
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
-    changeOrigin: true
-    // pathRewrite: {
-    //   // 通过路径重写，去除请求路径中的 `/backend`
-    //   // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-    //   '^/backend/': '/'
-    // }
+    changeOrigin: true,
+    pathRewrite: {
+      // 通过路径重写，去除请求路径中的 `/backend`
+      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      '^/backend/': '/'
+    }
   })(req, res)
 }
